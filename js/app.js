2bc22924be287e4816ecf03d22097c733f98ba67
Luis Fernando Florian <lflorian@mega.corp>
2018-08-06 10:19:03 -0600

    Desarrollo para dibujar en un canvas

---
+++ js/app.js
@@ -0,0 +1,78 @@
+var movimientos = new Array();
+var pulsado;
+
+function createScreen() {
+    var canvasDiv = document.getElementById('screen');
+    canvas = document.createElement('canvas');
+    canvas.setAttribute('width', 200);
+    canvas.setAttribute('height', 200);
+    canvas.setAttribute('id', 'canvas');
+    canvas.setAttribute('class','CanvasClass');
+    canvasDiv.appendChild(canvas);
+    if(typeof G_vmlCanvasManager != 'undefined') {
+        canvas = G_vmlCanvasManager.initElement(canvas);
+    }
+    context = canvas.getContext("2d");
+
+    $('#canvas').mousedown(function (e) {
+        pulsado = true;
+        movimientos.push([e.pageX - this.offsetLeft,
+        e.pageY - this.offsetTop,
+            false]);
+        reWrite();
+    });
+    
+    $('#canvas').mousemove(function (e) {
+        if (pulsado) {
+            movimientos.push([e.pageX - this.offsetLeft,
+            e.pageY - this.offsetTop,
+                true]);
+            reWrite();
+        }
+    });
+    
+    $('#canvas').mouseup(function (e) {
+        pulsado = false;
+    });
+    
+    $('#canvas').mouseleave(function (e) {
+        pulsado = false;
+    });
+
+
+    $('#canvas').bind('touchstart', function (event) {
+        var e = event.originalEvent;
+        e.preventDefault();
+        pulsado = true;
+        movimientos.push([e.targetTouches[0].pageX - this.offsetLeft,
+        e.targetTouches[0].pageY - this.offsetTop,
+            false]);
+        repinta();
+    });
+
+    reWrite();
+}
+
+function reWrite() {
+    canvas.width = canvas.width; // Limpia el lienzo
+
+    context.strokeStyle = "#0000a0";
+    context.lineJoin = "round";
+    context.lineWidth = 6;
+
+    for (var i = 0; i < movimientos.length; i++) {
+        context.beginPath();
+        if (movimientos[i][2] && i) {
+            context.moveTo(movimientos[i - 1][0], movimientos[i - 1][1]);
+        } else {
+            context.moveTo(movimientos[i][0], movimientos[i][1]);
+        }
+        context.lineTo(movimientos[i][0], movimientos[i][1]);
+        context.closePath();
+        context.stroke();
+    }
+}
+
+$('.botonCarga').click(function() {
+    var signature = canvas.toDataURL();
+});
